f50993df67180753c937d0b1848f87eb93e8e983
Fidentino <tokar.illya00@gmail.com>
2021-01-20 04:34:27 +0200

    Final project version
    Scenes and scripts finished, temporary files removed

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using System.Dynamic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
@@ -13,43 +15,141 @@ public class Main : MonoBehaviour
     public Tilemap grid;
     public Tile white;
     public Tile black;
-    public TileBase empty;
-    int N, cellsize, width;
-    bool[,,] matrix;
+    public Button button;
+    public GameObject[] letters;
+    int N, cellsize, width, levelsPlayed, points, lives;
+    NeuralNetwork net = new NeuralNetwork();
 
-    void fill(Tile tile, int X1, int Y1, int X2, int Y2)
+    string textFile = "words.csv";
+
+    string word, translation;
+    string[] lines;
+
+    void Fill(Tile tile, int X1, int Y1, int X2, int Y2)
     {
         grid.BoxFill(new Vector3Int(X1, Y1, 0), tile, X1, Y1, X2, Y2);
     }
 
-    Vector3Int mouseToGrid()
+    double[,] CellMap(int leftBorder)
     {
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        return grid.WorldToCell(mouseWorldPos);
+        bool[,] matrix = new bool[cellsize + 1, cellsize + 1];
+        int l = -1, r = -1, t = -1, b = -1;
+        for (int i = 0; i < cellsize + 1; i++)
+            for (int j = 0; j < cellsize + 1; j++)
+                if (grid.GetTile(new Vector3Int(leftBorder + j, -cellsize / 2 + i, 0)) == black)
+                {
+                    matrix[i, j] = true;
+                    if (l == -1 || l > j) l = j;
+                    if (t == -1 || t > i) t = i;
+                    if (r < j) r = j;
+                    if (b < i) b = i;
+                }
+                else
+                    matrix[i, j] = false;
+        double[,] map = new double[16, 16];
+        for (int i = 0; i < 16; i++)
+            for (int j = 0; j < 16; j++)
+                map[i, j] = 0;
+        double dw = (double)(r - l + 1) / 16;
+        double dh = (double)(b - t + 1) / 16;
+        for (int i = 0; i < b - t + 1; i++)
+            for (int j = 0; j < r - l + 1; j++)
+                if (grid.GetTile(new Vector3Int(leftBorder + l + j, -cellsize / 2 + t + i, 0)) == black)
+                {
+                    int c = (int)Math.Floor(i / dh);
+                    int d = (int)Math.Floor(j / dw);
+                    map[c, d] += (Math.Min(i + 1, (c + 1) * dh) - i) * (Math.Min(j + 1, (d + 1) * dw) - j);
+                    if (i + 1 > (c + 1) * dh)
+                        map[c + 1, d] += ((i + 1) - (c + 1) * dh) * (Math.Min(j + 1, (d + 1) * dw) - j);
+                    if (j + 1 > (d + 1) * dw)
+                        map[c, d + 1] += (Math.Min(i + 1, (c + 1) * dh) - i) * ((j + 1) - (d + 1) * dw);
+                    if (i + 1 > (c + 1) * dh && j + 1 > (d + 1) * dw)
+                        map[c + 1, d + 1] += ((i + 1) - (c + 1) * dh) * ((j + 1) - (d + 1) * dw);
+                }
+        double max = 0;
+        for (int i = 0; i < 16; i++)
+            for (int j = 0; j < 16; j++)
+                if (map[i, j] > max)
+                    max = map[i, j];
+        max /= 2;
+        if (max != 0)
+            for (int i = 0; i < 16; i++)
+                for (int j = 0; j < 16; j++)
+                    map[i, j] /= max;
+
+        return map;
+    }
+
+    Vector3Int ScreenToCell(Vector3 position)
+    {
+        return grid.WorldToCell(Camera.main.ScreenToWorldPoint(position));
     }
 
     void Start()
     {
-        Application.targetFrameRate = 300;
-        N = 8;
-        width = grid.WorldToCell(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0))).x * 2 - 40;
+        lines = File.ReadAllLines(textFile);
+        button.onClick.AddListener(delegate {
+            ButtonClicked(button);
+        });
+        levelsPlayed = 0;
+        points = 100;
+        lives = 3;
+        GameObject.Find("PointsNumber").GetComponent<Text>().text = points.ToString();
+        GameObject.Find("LivesNumber").GetComponent<Text>().text = lives.ToString();
+        NewLevel();
+    }
+
+    void NewLevel()
+    {
+        System.Random rnd = new System.Random();
+        int line;
+        do
+        {
+            line = rnd.Next(lines.Length);
+        } while (lines[line].Length == 0); 
+        string[] pair = lines[line].Split(';');
+        word = pair[0];
+        translation = pair[1];
+        GameObject.Find("Word").GetComponent<Text>().text = word;
+        lines[line] = "";
+        levelsPlayed++;
+        GameObject.Find("LevelNumber").GetComponent<Text>().text = levelsPlayed.ToString();
+        grid.ClearAllTiles();
+        N = translation.Length;
+        width = ScreenToCell(new Vector3(Screen.width, Screen.height, 0)).x * 2 - 40;
         cellsize = (width - 20 * (N - 1)) / N;
-        matrix = new bool[N, cellsize + 1, cellsize + 1];
         grid.size = new Vector3Int(width, cellsize, 0);
+        Font arial;
+        arial = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+        letters = new GameObject[N];
         for (int i = 0; i < N; i++)
         {
-            fill(white, -width / 2 + (20 + cellsize) * i, -cellsize / 2, -width / 2 + (20 + cellsize) * i + cellsize, cellsize / 2);
-            for (int j = 0; j <= cellsize; j++)
-                for (int k = 0; k <= cellsize; k++)
-                    matrix[i, j, k] = false;
+            Fill(white, -width / 2 + (20 + cellsize) * i, -cellsize / 2, -width / 2 + (20 + cellsize) * i + cellsize, cellsize / 2);
+            letters[i] = new GameObject("letter" + i);
+            letters[i].transform.parent = GameObject.Find("Canvas").transform;
+            letters[i].AddComponent<RectTransform>();
+            Vector3 letterCoords = grid.CellToWorld(new Vector3Int((-width / 2 + (20 + cellsize) * i + cellsize / 2) * 3, -cellsize * 2, 0));
+            RectTransform RT = letters[i].GetComponent<RectTransform>();
+            RT.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            RT.anchoredPosition = letterCoords;
+            RT.sizeDelta = new Vector2(30, 30);
+            letters[i].AddComponent<Text>();
+            Text T = letters[i].GetComponent<Text>();
+            T.font = arial;
+            T.fontSize = 20;
+            T.fontStyle = FontStyle.Bold;
+            T.color = Color.white;
+            T.alignment = TextAnchor.MiddleCenter;
+
         }
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        int x = mouseToGrid().x;
-        int y = mouseToGrid().y;
+        int x = ScreenToCell(Input.mousePosition).x;
+        int y = ScreenToCell(Input.mousePosition).y;
         if (Input.GetMouseButton(0))
         {
             for (int i = -5; i <= 5; i++)
@@ -73,57 +173,64 @@ public class Main : MonoBehaviour
         }
         for (int i = 0; i < N; i++)
         {
-            int border = -width / 2 + (20 + cellsize) * i;
-            int l = -1, r = -1, t = -1, b = -1;
-            for (int j = 0; j < cellsize + 1; j++)
-                for (int k = 0; k < cellsize + 1; k++)
-                    if (grid.GetTile(new Vector3Int(border + k, -cellsize / 2 + j, 0)) == black)
-                    {
-                        matrix[i, j, k] = true;
-                        if (l == -1 || l < k) l = k;
-                        if (t == -1 || t < j) t = j;
-                        if (r < k) r = k;
-                        if (b < j) b = j;
-                    }
-                    else
-                        matrix[i, j, k] = false;
-            double[,] trim = new double[16, 16];
-            for (int j = 0; j < 16; j++)
-                for (int k = 0; k < 16; k++)
-                    trim[j, k] = 0;
-            double dw = (double) (r - l + 1) / 16;
-            double dh = (double) (b - t + 1) / 16;
-            for (int j = 0; j < b - t + 1; j++)
-                for (int k = 0; k < r - l + 1; k++)
-                    if (grid.GetTile(new Vector3Int(border + l + k, -cellsize / 2 + t + j, 0)) == black)
-                    {
-                        int c = (int) Math.Floor(j / dh);
-                        int d = (int) Math.Floor(k / dw);
-                        trim[c, d] += (Math.Min(j + 1, (c + 1) * dh) - j) * (Math.Min(k + 1, (d + 1) * dw) - k);
-                        if (j + 1 > (c + 1) * dh)
-                            trim[c + 1, d] += ((j + 1) - (c + 1) * dh) * (Math.Min(k + 1, (d + 1) * dw) - k);
-                        if (k + 1 > (d + 1) * dw)
-                            trim[c, d + 1] += (Math.Min(j + 1, (c + 1) * dh) - j) * ((k + 1) - (d + 1) * dw);
-                        if (j + 1 > (c + 1) * dh && k + 1 > (d + 1) * dw)
-                            trim[c + 1, d + 1] += ((j + 1) - (c + 1) * dh) * ((k + 1) - (d + 1) * dw);
-                    }
-            double max = 0;
-            for (int j = 0; j < 16; j++)
-                for (int k = 0; k < 16; k++)
-                    if (trim[j, k] > max)
-                        max = trim[j, k];
-            max /= 2;
-            bool[,] map = new bool[16, 16];
-            for (int j = 0; j < 16; j++)
-                for (int k = 0; k < 16; k++)
-                {
-                    map[j, k] = (trim[j, k] >= max && max > 0);
-                    if (map[j, k])
-                        grid.SetTile(new Vector3Int(1000 + 20 * i + k, 1000 + j, 0), black);
-                    else
-                        grid.SetTile(new Vector3Int(1000 + 20 * i + k, 1000 + j, 0), white);
-
-                }
+            double[,] map = CellMap(-width / 2 + (20 + cellsize) * i);
+            double[] results = net.Calculate(map);
+            int max = 0;
+            for (int j = 1; j < 26; j++)
+                if (results[j] > results[max]) max = j;
+            if (results[max] != 0)
+                GameObject.Find("letter" + i).GetComponent<Text>().text = ('A' + max).ToString();
+            else
+                GameObject.Find("letter" + i).GetComponent<Text>().text = "";
         }
+    }
+
+    void ButtonClicked(Button button)
+    {
+        string result = "";
+        for (int i = 0; i < N; i++)
+        { 
+            result += GameObject.Find("letter" + i).GetComponent<Text>().text;
+            int left = -width / 2 + (20 + cellsize) * i;
+            int right = left + cellsize;
+            for (int j = left; j <= right; j++)
+                for (int k = -cellsize / 2; k <= cellsize / 2; k++)
+                    grid.SetTile(new Vector3Int(j, k, 0), white);
+        }
+        if (String.Compare(result.ToUpper(), translation.ToUpper()) == 0)
+        {
+            lives++;
+            points += 100;
+            GameObject.Find("PointsNumber").GetComponent<Text>().text = points.ToString();
+            GameObject.Find("LivesNumber").GetComponent<Text>().text = lives.ToString();
+            for (int i = 0; i < N; i++)
+                UnityEngine.Object.Destroy(GameObject.Find("letter" + i));
+            if (levelsPlayed < lines.Length)
+                NewLevel();
+            else
+                GameOver(true);
+        }
+        else
+        {
+            lives--;
+            points -= 50;
+            GameObject.Find("PointsNumber").GetComponent<Text>().text = points.ToString();
+            GameObject.Find("LivesNumber").GetComponent<Text>().text = lives.ToString();
+            if (lives == 0)
+                GameOver(false);
+        }
+    }
+
+    void GameOver(bool value)
+    {
+        grid.ClearAllTiles();
+            for (int i = 0; i < N; i++)
+                UnityEngine.Object.Destroy(GameObject.Find("letter" + i));
+        GameObject.Find("LivesLabel").GetComponent<Text>().text = "";
+        GameObject.Find("LivesNumber").GetComponent<Text>().text = "";
+        GameObject.Find("LevelLabel").GetComponent<Text>().text = "";
+        GameObject.Find("LevelNumber").GetComponent<Text>().text = "";
+        UnityEngine.Object.Destroy(GameObject.Find("Button"));
+        GameObject.Find("Word").GetComponent<Text>().text = (value ? "VICTORY" : "GAME OVER");
     }
 }
---
+++ Assets/Scripts/Training.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Funcs
+{
+    static public double MSE(double[] actual, double[] ideal)
+    {
+        double val = 0;
+        for (int i = 0; i < actual.Length; i++)
+            val += (actual[i] - ideal[i]) * (actual[i] - ideal[i]);
+        return 0.5 * val / actual.Length;
+    }
+
+    static public double sigmoid(double x)
+    {
+        return 1.0 / (1.0 + Math.Exp(-x));
+    }
+
+    static public double deriv(double f)
+    {
+        return f * (1.0 - f);
+    }
+}
+
+public class NeuronLayer
+{
+    public double[] inVal;
+    public double[] outVal;
+
+    public NeuronLayer(int size)
+    {
+        inVal = new double[size];
+        outVal = new double[size];
+    }
+}
+
+public class NeuralNetwork
+{
+    public NeuronLayer hidden;
+    public NeuronLayer output;
+    public double[,] inHidden;
+    public double[,] hiddenOut;
+
+    public double learningSpeed;
+
+    static string textFile = "weights.csv";
+
+    public NeuralNetwork()
+    {
+        hidden = new NeuronLayer(20);
+        output = new NeuronLayer(26);
+        inHidden = new double[256, 20];
+        hiddenOut = new double[20, 26];
+
+        string[] lines = File.ReadAllLines(textFile);
+        for (int i = 0; i < 256; i++)
+        {
+            string[] numbers = lines[i].Split(';');
+            for (int j = 0; j < 20; j++)
+                inHidden[i, j] = Convert.ToDouble(numbers[j]);
+        }
+        for (int i = 0; i < 20; i++)
+        {
+            string[] numbers = lines[256 + i].Split(';');
+            for (int j = 0; j < 26; j++)
+                hiddenOut[i, j] = Convert.ToDouble(numbers[j]);
+        }
+
+        UnityEngine.Debug.Log(inHidden[25, 15]);
+        learningSpeed = 0.5;
+    }
+
+    public double[] Calculate(double[,] input)
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            hidden.inVal[i] = 0;
+            for (int j = 0; j < 16; j++)
+                for (int k = 0; k < 16; k++)
+                    hidden.inVal[i] += inHidden[j * 16 + k, i] * input[j, k];
+            hidden.outVal[i] = Funcs.sigmoid(hidden.inVal[i]);
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            output.inVal[i] = 0;
+            for (int j = 0; j < 20; j++)
+                output.inVal[i] += hiddenOut[j, i] * hidden.outVal[j];
+            output.inVal[i] = Funcs.sigmoid(output.outVal[i]);
+        }
+
+        //UnityEngine.Debug.Log(String.Join(";", output.outVal));
+        return output.outVal;
+    }
+    public void BackPropagate(double[,] input, double[] ideal)
+    {
+        double error;
+
+        error = Funcs.MSE(Calculate(input), ideal);
+
+        while (error > 0.01)
+        {
+            UnityEngine.Debug.Log(error);
+            double[] dOut = new double[26];
+            double[] dHidden = new double[20];
+
+            for (int i = 0; i < 26; i++)
+                dOut[i] = (ideal[i] - output.outVal[i]) * Funcs.deriv(output.inVal[i]);
+
+            for (int i = 0; i < 20; i++)
+            {
+                dHidden[i] = 0;
+                for (int j = 0; j < 26; j++)
+                    dHidden[i] += hiddenOut[i, j] * dOut[j];
+                dHidden[i] *= Funcs.deriv(hidden.inVal[i]);
+            }
+
+            for (int i = 0; i < 20; i++)
+                for (int j = 0; j < 26; j++)
+                    hiddenOut[i, j] += learningSpeed * dOut[j] * hidden.outVal[i];
+
+            for (int i = 0; i < 16; i++)
+                for (int j = 0; j < 16; j++)
+                    for (int k = 0; k < 20; k++)
+                        inHidden[i * 16 + j, k] += learningSpeed * dHidden[k] * input[i, j];
+
+            error = Funcs.MSE(Calculate(input), ideal);
+        }
+
+        File.Delete(textFile);
+        string[] lines = new string[276];
+        for (int i = 0; i < 256; i++)
+        {
+            string[] numbers = new string[20];
+            for (int j = 0; j < 20; j++)
+                numbers[j] = inHidden[i, j].ToString();
+            lines[i] = String.Join(";", numbers);
+        }
+        for (int i = 0; i < 20; i++)
+        {
+            string[] numbers = new string[20];
+            for (int j = 0; j < 20; j++)
+                numbers[j] = hiddenOut[i, j].ToString();
+            lines[256 + i] = String.Join(";", numbers);
+        }
+        File.WriteAllLines(textFile, lines);
+
+    }
+
+}
+
+public class Training : MonoBehaviour
+{
+    public Tilemap grid;
+    public Tile white;
+    public Tile black;
+    public TileBase empty;
+    public InputField textField;
+    public Button button;
+    public Toggle toggle;
+    public NeuralNetwork net;
+
+
+    void Fill(Tile tile, int X1, int Y1, int X2, int Y2)
+    {
+        grid.BoxFill(new Vector3Int(X1, Y1, 0), tile, X1, Y1, X2, Y2);
+    }
+
+    double[,] CellMap()
+    {
+        bool[,] matrix = new bool[81, 81];
+        int l = -1, r = -1, t = -1, b = -1;
+        for (int i = 0; i < 81; i++)
+            for (int j = 0; j < 81; j++)
+                if (grid.GetTile(new Vector3Int(-240 + j, -40 + i, 0)) == black)
+                {
+                    matrix[i, j] = true;
+                    if (l == -1 || l > j) l = j;
+                    if (t == -1 || t > i) t = i;
+                    if (r < j) r = j;
+                    if (b < i) b = i;
+                }
+                else
+                    matrix[i, j] = false;
+        double[,] map = new double[16, 16];
+        for (int i = 0; i < 16; i++)
+            for (int j = 0; j < 16; j++)
+                map[i, j] = 0;
+        double dw = (double)(r - l + 1) / 16;
+        double dh = (double)(b - t + 1) / 16;
+        for (int i = 0; i < b - t + 1; i++)
+            for (int j = 0; j < r - l + 1; j++)
+                if (grid.GetTile(new Vector3Int(-240 + l + j, -40 + t + i, 0)) == black)
+                {
+                    int c = (int)Math.Floor(i / dh);
+                    int d = (int)Math.Floor(j / dw);
+                    map[c, d] += (Math.Min(i + 1, (c + 1) * dh) - i) * (Math.Min(j + 1, (d + 1) * dw) - j);
+                    if (i + 1 > (c + 1) * dh)
+                        map[c + 1, d] += ((i + 1) - (c + 1) * dh) * (Math.Min(j + 1, (d + 1) * dw) - j);
+                    if (j + 1 > (d + 1) * dw)
+                        map[c, d + 1] += (Math.Min(i + 1, (c + 1) * dh) - i) * ((j + 1) - (d + 1) * dw);
+                    if (i + 1 > (c + 1) * dh && j + 1 > (d + 1) * dw)
+                        map[c + 1, d + 1] += ((i + 1) - (c + 1) * dh) * ((j + 1) - (d + 1) * dw);
+                }
+        double max = 0;
+        for (int i = 0; i < 16; i++)
+            for (int j = 0; j < 16; j++)
+                if (map[i, j] > max)
+                    max = map[i, j];
+        max /= 2;
+        for (int i = 0; i < 16; i++)
+            for (int j = 0; j < 16; j++)
+            {
+                if (max != 0)
+                    map[i, j] /= max;
+                if (map[i, j] >= 0.5)
+                    grid.SetTile(new Vector3Int(-7 + j, -7 + i, 0), black);
+                else
+                    grid.SetTile(new Vector3Int(-7 + j, -7 + i, 0), white);
+            }
+
+
+        return map;
+    }
+    Vector3Int ScreenToCell(Vector3 position)
+    {
+        return grid.WorldToCell(Camera.main.ScreenToWorldPoint(position));
+    }
+
+    /*public PointerEventData toggleClick()
+    {
+
+    }*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        net = new NeuralNetwork();
+        toggle.onValueChanged.AddListener(delegate {
+            ToggleValueChanged(toggle);
+        });
+        button.onClick.AddListener(delegate {
+            ButtonClicked(button);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int x = ScreenToCell(Input.mousePosition).x;
+        int y = ScreenToCell(Input.mousePosition).y;
+        if (Input.GetMouseButton(0))
+        {
+            for (int i = -5; i <= 5; i++)
+                for (int j = -5; j <= 5; j++)
+                    if ((i * i + j * j < 25) && grid.GetTile(new Vector3Int(x + i, y + j, 0)) == white)
+                        grid.SetTile(new Vector3Int(x + i, y + j, 0), black);
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            if (-240 <= x && x <= -160 && -40 <= y && y <= 40)
+            {
+                for (int i = -240; i <= -160; i++)
+                    for (int j = -40; j <= 40; j++)
+                        grid.SetTile(new Vector3Int(i, j, 0), white);
+            }
+        }
+        if (!toggle.isOn)
+        {
+            double[] results = net.Calculate(CellMap());
+            int max = 0;
+            for (int i = 1; i < 26; i++)
+                if (results[i] > results[max]) max = i;
+            if (results[max] != 0)
+                textField.textComponent.text = ('A' + max).ToString();
+            else
+                textField.textComponent.text = "";
+        }
+    }
+
+    void ToggleValueChanged(Toggle toggle)
+    {
+        textField.readOnly = !textField.readOnly;
+        button.interactable = !button.interactable;
+        textField.textComponent.text = "";
+        UnityEngine.Debug.Log("toggle");
+    }
+
+    void ButtonClicked(Button button)
+    {
+        UnityEngine.Debug.Log("button");
+        int ch = textField.text.ToUpper()[0] - 'A';
+        double[] trainingResults = new double[26];
+        for (int i = 0; i < 26; i++)
+            trainingResults[i] = (i == ch ? 0.99 : 0.01);
+        net.BackPropagate(CellMap(), trainingResults);
+        for (int i = -240; i <= -160; i++)
+            for (int j = -40; j <= 40; j++)
+                grid.SetTile(new Vector3Int(i, j, 0), white);
+    }
+}
